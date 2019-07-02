caf8e15f194f8fa38fc04077cb30a8801d8440c0
Felix Kunz <felix.kunzjr@gmail.com>
2019-07-02 09:31:41 +0200

    Code

---
+++ Code/Pneumatik/SolenoidRelais/SolenoidRelais.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(49,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(49,HIGH);
+delay(1000);
+digitalWrite(49,LOW);
+delay(1000);
+}
